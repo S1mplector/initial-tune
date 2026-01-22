18114111724e7e4f8bb99dcf2f562c4a0c17b893
S1mplector <mehmetogluilgaz07@gmail.com>
2026-01-23 00:34:40 +0100

    Add top-left back button to Controls screen for improved navigation

---
+++ scenes/menu/TopLeftBackControls.gd
@@ -0,0 +1,19 @@
+extends Button
+
+func _ready():
+	pressed.connect(_on_pressed)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		go_back()
+
+func _on_pressed():
+	go_back()
+
+func go_back():
+	var main_menu_button = get_node("/root/GameOption").main_menu_button.instantiate()
+	var screen = get_tree().get_root().find_child("Screen", true, false)
+	var controls = get_tree().get_root().find_child("Controls", true, false)
+	if screen and controls:
+		screen.add_child(main_menu_button)
+		controls.queue_free()
